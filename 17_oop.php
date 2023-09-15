4b037b5cf34d997e8ce41205b897b0307ca4fdad
Christian <christiandarlington0@gmail.com>
2023-09-15 01:13:58 -0400

    php project inside of feedback folder

--- 17_oop.php
+++ 17_oop.php
@@ -5,11 +5,23 @@ class User {
   public $name;
   public $email;
   public $password;
+
+  // A constructor is a method that runs when a object is created
+  public function __construct(){
+    echo 'constructor ran';
+  }
+
+  //Method is a function that belongs to a class
+  function set_name($name){
+    $this->name = $name;
+  }
+
+  function get_name(){
+    return $this->name;
+  }
 }
 
 // Instatiate user object
 $user1 = new User();
+$user2 = new User();
 
-$user1->name = 'Brad';
-
-var_dump($user1);
